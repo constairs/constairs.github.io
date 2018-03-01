2bacb7198c775798b051ebb3fb920c5453baae5b
constairs <constairs@gmail.com>
2018-03-01 13:24:38 +0700

    Fixed styles on works pages

--- js/common.js
+++ js/common.js
@@ -32,77 +32,6 @@ $(function() {
 		$(this).addClass("active");
 	});
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	/*$("form").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			$(".modal-complete, .ten").fadeIn(250);
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	}); */
-
-	// $("form").submit(function(){
-	// 	var dat, fm, valid_name, valid_email;
-	// 	fm = $(this);
-	// 	dat = fm.serialize();
-	// 	valid_name = fm.find('.validname');
-	// 	valid_email = fm.find('.validemail');
-	// 	valid_message = fm.find('.validmessage');
-	// 	valid_name.css("border-color","#ccc");
-	// 	valid_email.css("border-color","#ccc");
-	// 	valid_message.css("border-color","#ccc");
-
-	// 	valid_name.blur(function() {
-	// 		if(valid_name.val().length>1) {
-	// 			$(this).css("border-color","rgba(86,155,68,.8)");
-	// 		} else {
-	// 			$(this).css("border-color","rgba(243,44,45,.8)");
-	// 		}
-	// 	});
-
-	// 	valid_email.blur(function() {
-	// 		if($(this).val() != '') {
-	// 			var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-	// 			if(pattern.test($(this).val())){
-	// 				$(this).css("border-color","rgba(86,155,68,.8)");
-	// 				valid = 1;
-	// 			} else {
-	// 				$(this).css("border-color","rgba(243,44,45,.8)");
-	// 			}
-	// 		} else {
-	// 			$(this).css("border-color","rgba(243,44,45,.8)");
-	// 		}
-	// 	});
-
-	// 	if(valid_name.val().length>1 && valid_email.val().length>3){
-	// 		$.post("mail.php", {dat:dat},function(data) {
-	// 			$(".modal-complete, .ten").fadeIn(250);
-	// 			setTimeout(function() {
-	// 				fm.trigger("reset");
-	// 			}, 1000);
-	// 		});
-	// 		return false;
-	// 	}else{
-	// 		if(valid_name.val().length<1){
-	// 			valid_name.css("border-color","rgba(243,44,45,.8)");
-	// 		}            
-	// 		if(valid_email.val().length<3){
-	// 			valid_email.css("border-color","rgba(243,44,45,.8)");
-	// 		}
-	// 		return false; 
-	// 	}
-	// }); // end validate
-
-
 	$("#form").validate({
 		submitHandler: function(form) {
 
@@ -147,4 +76,4 @@ $(function() {
 		}
 	});
 
-});// end ready
+}); // end ready
